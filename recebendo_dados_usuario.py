174bae02dcaf3cf5a37c2790aef77141cdc31777
caiomatheus-dev <caiomatheus.dev@gmail.com>
2025-12-05 01:50:30 -0300

    feat: adicionar aula01 - recebendo dados do usuário

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,17 @@
+name = input('Whats your name man ? ')
+print(f'Welcome {name}')
+
+age = int(input('How old are you ? '))
+print(f'the {name} born in {2025 - age} years old')
+
+sport = input('Whats sport do you play ? ')
+print(f'{sport} Its a great sport, congratulations !!')
+
+food = input('Whats favorite food ? ')
+print(f'{food} its very very incrible !!')
+
+
+
+
+
+
